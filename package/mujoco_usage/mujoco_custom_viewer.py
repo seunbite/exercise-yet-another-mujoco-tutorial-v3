f5323b8f386bf93ccdcde18f334c86cd04d74200
seunbite <phone4775137809@gmail.com>
2025-06-14 22:17:55 +0900

    human, furniture, and robot cleaner

--- package/mujoco_usage/mujoco_custom_viewer.py
+++ package/mujoco_usage/mujoco_custom_viewer.py
@@ -230,7 +230,7 @@ class MujocoMinimalViewer(MinimalCallbacks):
         g.objtype = mujoco.mjtObj.mjOBJ_UNKNOWN
         g.objid = -1
         g.category = mujoco.mjtCatBit.mjCAT_DECOR
-        g.texid = -1
+        # g.texid = -1  # texid removed in MuJoCo 3.x
         g.texuniform = 0
         g.texrepeat[0] = 1
         g.texrepeat[1] = 1
